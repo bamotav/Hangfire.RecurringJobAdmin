da6a5996381e370f91f9673c0e5e9bd88381eb23
Jadhiel Vélez <jadhielv@gmail.com>
2020-10-06 13:29:43 -0400

    Remove unused property

--- src/Hangfire.RecurringJobAdmin/PeriodicJobBuilder.cs
+++ src/Hangfire.RecurringJobAdmin/PeriodicJobBuilder.cs
@@ -1,28 +1,21 @@
 ﻿using Hangfire.RecurringJobAdmin.Core;
 using Hangfire.States;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 
 namespace Hangfire.RecurringJobAdmin
 {
-
     internal static class PeriodicJobBuilder
     {
-        public static List<RecurringJobAttribute> Metadata { get; private set; }
         internal static void GetAllJobs()
         {
-            Metadata = new List<RecurringJobAttribute>();
-
             foreach (var assembly in StorageAssemblySingleton.GetInstance().currentAssembly)
             {
                 foreach (var type in assembly.GetTypes())
                 {
                     foreach (var method in type.GetTypeInfo().DeclaredMethods)
                     {
-
                         if (!method.IsDefined(typeof(RecurringJobAttribute), false)) continue;
 
                         var attribute = method.GetCustomAttribute<RecurringJobAttribute>(false);
@@ -32,7 +25,6 @@ namespace Hangfire.RecurringJobAdmin
                         if (method.GetCustomAttributes(true).OfType<RecurringJobAttribute>().Any())
                         {
                             var attr = method.GetCustomAttribute<RecurringJobAttribute>();
-                            Metadata.Add(attr);
                         }
 
                         var _registry = new RecurringJobRegistry();
@@ -43,12 +35,9 @@ namespace Hangfire.RecurringJobAdmin
                                   attribute.Cron,
                                   string.IsNullOrEmpty(attribute.TimeZone) ? TimeZoneInfo.Utc : TimeZoneInfo.FindSystemTimeZoneById(attribute.TimeZone),
                                   attribute.Queue ?? EnqueuedState.DefaultQueue);
-
                     }
-
                 }
             }
-
         }
     }
 }
